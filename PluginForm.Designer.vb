5f8c75fa2257e9929596520723ae79bd0e303f61
Eldar Gerfanov <zero.divide.net@gmail.com>
2020-10-04 04:18:59 -0400

    Add things

---
+++ PluginForm.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class PluginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.BTN_0001 = New System.Windows.Forms.Button()
+        Me.BTN_001 = New System.Windows.Forms.Button()
+        Me.BTN_01 = New System.Windows.Forms.Button()
+        Me.BTN_X = New System.Windows.Forms.Button()
+        Me.BTN_Y = New System.Windows.Forms.Button()
+        Me.BTN_Z = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.MPG_BOX = New System.Windows.Forms.PictureBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        CType(Me.MPG_BOX, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.AutoSize = True
+        Me.TableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.BTN_0001, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.BTN_001, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.BTN_01, 0, 2)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 48)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(65, 105)
+        Me.TableLayoutPanel1.TabIndex = 1
+        '
+        'BTN_0001
+        '
+        Me.BTN_0001.AutoSize = True
+        Me.BTN_0001.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_0001.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_0001.ForeColor = System.Drawing.Color.White
+        Me.BTN_0001.Location = New System.Drawing.Point(3, 3)
+        Me.BTN_0001.Name = "BTN_0001"
+        Me.BTN_0001.Size = New System.Drawing.Size(59, 29)
+        Me.BTN_0001.TabIndex = 0
+        Me.BTN_0001.Text = "0.001"
+        Me.BTN_0001.UseVisualStyleBackColor = True
+        '
+        'BTN_001
+        '
+        Me.BTN_001.AutoSize = True
+        Me.BTN_001.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_001.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_001.ForeColor = System.Drawing.Color.White
+        Me.BTN_001.Location = New System.Drawing.Point(3, 38)
+        Me.BTN_001.Name = "BTN_001"
+        Me.BTN_001.Size = New System.Drawing.Size(59, 29)
+        Me.BTN_001.TabIndex = 0
+        Me.BTN_001.Text = "0.010"
+        Me.BTN_001.UseVisualStyleBackColor = True
+        '
+        'BTN_01
+        '
+        Me.BTN_01.AutoSize = True
+        Me.BTN_01.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_01.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_01.ForeColor = System.Drawing.Color.White
+        Me.BTN_01.Location = New System.Drawing.Point(3, 73)
+        Me.BTN_01.Name = "BTN_01"
+        Me.BTN_01.Size = New System.Drawing.Size(59, 29)
+        Me.BTN_01.TabIndex = 0
+        Me.BTN_01.Text = "0.100"
+        Me.BTN_01.UseVisualStyleBackColor = True
+        '
+        'BTN_X
+        '
+        Me.BTN_X.AutoSize = True
+        Me.BTN_X.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_X.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_X.ForeColor = System.Drawing.Color.White
+        Me.BTN_X.Location = New System.Drawing.Point(3, 3)
+        Me.BTN_X.Name = "BTN_X"
+        Me.BTN_X.Padding = New System.Windows.Forms.Padding(5)
+        Me.BTN_X.Size = New System.Drawing.Size(40, 39)
+        Me.BTN_X.TabIndex = 0
+        Me.BTN_X.Text = "X"
+        Me.BTN_X.UseVisualStyleBackColor = True
+        '
+        'BTN_Y
+        '
+        Me.BTN_Y.AutoSize = True
+        Me.BTN_Y.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_Y.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_Y.ForeColor = System.Drawing.Color.White
+        Me.BTN_Y.Location = New System.Drawing.Point(49, 3)
+        Me.BTN_Y.Name = "BTN_Y"
+        Me.BTN_Y.Padding = New System.Windows.Forms.Padding(5)
+        Me.BTN_Y.Size = New System.Drawing.Size(39, 39)
+        Me.BTN_Y.TabIndex = 0
+        Me.BTN_Y.Text = "Y"
+        Me.BTN_Y.UseVisualStyleBackColor = True
+        '
+        'BTN_Z
+        '
+        Me.BTN_Z.AutoSize = True
+        Me.BTN_Z.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BTN_Z.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTN_Z.ForeColor = System.Drawing.Color.White
+        Me.BTN_Z.Location = New System.Drawing.Point(94, 3)
+        Me.BTN_Z.Name = "BTN_Z"
+        Me.BTN_Z.Padding = New System.Windows.Forms.Padding(5)
+        Me.BTN_Z.Size = New System.Drawing.Size(38, 39)
+        Me.BTN_Z.TabIndex = 0
+        Me.BTN_Z.Text = "Z"
+        Me.BTN_Z.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.AutoSize = True
+        Me.TableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel2.ColumnCount = 3
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel2.Controls.Add(Me.BTN_X, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.BTN_Y, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.BTN_Z, 2, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(71, 0)
+        Me.TableLayoutPanel2.Margin = New System.Windows.Forms.Padding(0)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(135, 45)
+        Me.TableLayoutPanel2.TabIndex = 2
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.AutoSize = True
+        Me.TableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel3.ColumnCount = 2
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel3.Controls.Add(Me.TableLayoutPanel2, 1, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.TableLayoutPanel1, 0, 1)
+        Me.TableLayoutPanel3.Controls.Add(Me.MPG_BOX, 1, 1)
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(-1, 3)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 2
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(206, 156)
+        Me.TableLayoutPanel3.TabIndex = 3
+        '
+        'MPG_BOX
+        '
+        Me.MPG_BOX.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.MPG_BOX.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MPG_BOX.Image = Global.MouseMPG.My.Resources.Resources.mpg
+        Me.MPG_BOX.Location = New System.Drawing.Point(74, 48)
+        Me.MPG_BOX.Name = "MPG_BOX"
+        Me.MPG_BOX.Size = New System.Drawing.Size(129, 105)
+        Me.MPG_BOX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.MPG_BOX.TabIndex = 3
+        Me.MPG_BOX.TabStop = False
+        '
+        'PluginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(459, 283)
+        Me.Controls.Add(Me.TableLayoutPanel3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "PluginForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Wheel MPG"
+        Me.TopMost = True
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
+        CType(Me.MPG_BOX, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As Windows.Forms.TableLayoutPanel
+    Friend WithEvents BTN_0001 As Windows.Forms.Button
+    Friend WithEvents BTN_001 As Windows.Forms.Button
+    Friend WithEvents BTN_01 As Windows.Forms.Button
+    Friend WithEvents BTN_X As Windows.Forms.Button
+    Friend WithEvents BTN_Y As Windows.Forms.Button
+    Friend WithEvents BTN_Z As Windows.Forms.Button
+    Friend WithEvents TableLayoutPanel2 As Windows.Forms.TableLayoutPanel
+    Friend WithEvents TableLayoutPanel3 As Windows.Forms.TableLayoutPanel
+    Friend WithEvents MPG_BOX As Windows.Forms.PictureBox
+End Class
---
+++ PluginForm.vb
@@ -0,0 +1,168 @@
+﻿Imports System.Threading
+Imports System.Windows.Forms
+
+Friend Class PluginForm
+    Private UC As Plugininterface.Entry
+    Dim PluginMain As UCCNCplugin
+    Dim MustClose As Boolean = False
+    Dim WheelDelta As Integer = 120
+    Dim zpos As Double = 0
+    Dim stp As Double = 0.001
+    Dim direction As Direction = Direction.X
+
+    Dim deftbcolor As Drawing.Color
+
+    Public Sub New(CallerPluginMain As UCCNCplugin)
+        Me.UC = CallerPluginMain.UC
+        Me.PluginMain = CallerPluginMain
+        InitializeComponent()
+    End Sub
+
+    Public Shared Sub Main()
+
+    End Sub
+
+    Private Sub PluginForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        WheelDelta = SystemInformation.MouseWheelScrollDelta
+        deftbcolor = BTN_X.BackColor
+        updateLabels()
+    End Sub
+
+    Private Sub PluginForm_FormClosing(sender As System.Object, e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Do not close the form when the red X button is pressed
+        ' But start a Thread which will stop the Loop call from the UCCNC
+        ' to prevent the form closing while there is a GUI update in the Loop event
+        If Not MustClose Then
+            e.Cancel = True
+            Dim thrClose As New Thread(Sub() Closeform())
+            thrClose.CurrentCulture = Thread.CurrentThread.CurrentCulture ' Preserve regional settings
+            thrClose.Start()
+        Else
+            ' Form is closing here...
+        End If
+    End Sub
+
+    Public Sub Closeform()
+        ' Stop the Loop event to update the GUI
+        PluginMain.LoopStop = True
+        ' Wait until the loop exited
+        While (PluginMain.LoopWorking)
+            Thread.Sleep(10)
+        End While
+        ' Set the mustclose variable to true and call the .Close() function to close the Form
+        MustClose = True
+        Me.Close()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_MouseWheel(sender As Object, e As MouseEventArgs)
+
+    End Sub
+
+    Private Sub PluginForm_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+
+
+    End Sub
+
+    Private Sub jog(steps As Integer)
+        If UC.IsMoving Or UC.Getfield(False, 54) Then
+            Return
+        End If
+
+        Dim dist As Double = steps * stp
+        zpos = zpos + dist
+        If direction = Direction.X Then
+            Dim v = UC.GetXmachpos()
+            UC.Code("G53 X" & v + dist)
+        End If
+        If direction = Direction.Y Then
+            Dim v = UC.GetYmachpos()
+            UC.Code("G53 Y" & v + dist)
+        End If
+        If direction = Direction.Z Then
+            Dim v = UC.GetZmachpos()
+            UC.Code("G53 Z" & v + dist)
+        End If
+        updateLabels()
+    End Sub
+
+    Private Sub updateLabels()
+        BTN_0001.BackColor = deftbcolor
+        BTN_001.BackColor = deftbcolor
+        BTN_01.BackColor = deftbcolor
+
+        BTN_X.BackColor = deftbcolor
+        BTN_Y.BackColor = deftbcolor
+        BTN_Z.BackColor = deftbcolor
+
+        If stp = 0.001 Then
+            BTN_0001.BackColor = Drawing.Color.Green
+        End If
+        If stp = 0.01 Then
+            BTN_001.BackColor = Drawing.Color.Green
+        End If
+        If stp = 0.1 Then
+            BTN_01.BackColor = Drawing.Color.Green
+        End If
+
+        If direction = Direction.X Then
+            BTN_X.BackColor = Drawing.Color.Green
+        End If
+
+        If direction = Direction.Y Then
+            BTN_Y.BackColor = Drawing.Color.Green
+        End If
+
+        If direction = Direction.Z Then
+            BTN_Z.BackColor = Drawing.Color.Green
+        End If
+
+
+    End Sub
+
+    Private Sub BTN_X_Click(sender As Object, e As EventArgs) Handles BTN_X.Click
+        direction = Direction.X
+        updateLabels()
+    End Sub
+    Private Sub BTN_y_Click(sender As Object, e As EventArgs) Handles BTN_Y.Click
+        direction = Direction.Y
+        updateLabels()
+    End Sub
+    Private Sub BTN_z_Click(sender As Object, e As EventArgs) Handles BTN_Z.Click
+        direction = Direction.Z
+        updateLabels()
+    End Sub
+
+    Private Sub BTN_0001_Click(sender As Object, e As EventArgs) Handles BTN_0001.Click
+        stp = 0.001
+        updateLabels()
+    End Sub
+
+    Private Sub BTN_001_Click(sender As Object, e As EventArgs) Handles BTN_001.Click
+        stp = 0.01
+        updateLabels()
+    End Sub
+
+    Private Sub BTN_01_Click(sender As Object, e As EventArgs) Handles BTN_01.Click
+        stp = 0.1
+        updateLabels()
+    End Sub
+
+    Private Sub MPG_BOX_Click(sender As Object, e As EventArgs) Handles MPG_BOX.Click
+
+    End Sub
+
+    Private Sub MPG_BOX_MouseWheel(sender As Object, e As MouseEventArgs) Handles MPG_BOX.MouseWheel
+        Dim steps As Integer = e.Delta / WheelDelta
+        jog(steps)
+    End Sub
+End Class
+
+Public Enum Direction
+    X
+    Y
+    Z
+End Enum
